67a72be7c2d962b0a5dc48858370d3ffddccc8fb
raka-arrayan <rakaarrayan27@gmail.com>
2026-01-12 10:34:11 +0700

    Fixing

---
+++ src/App_temp.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar_temp.jsx";
+import Welcome from "./Welcome.jsx";
+import Education from "./Education.jsx";
+import Experience from "./Experience.jsx";
+import Skill from "./Skills.jsx";
+import Project  from "./Project.jsx"; 
+import Contact from "./Contact.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Welcome />
+      <Education />
+      <Experience />
+      <Skill />
+      <Project />
+      <Contact />
+    </div>
+  );
+}
